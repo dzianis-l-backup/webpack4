a3d4d4bfdbd6f7f4ba8d3725863260c4290176cc
DziLean <gestaltldi@yandex.ru>
2018-03-20 08:39:16 +0300

    react preset

--- src/main.js
+++ src/main.js
@@ -3,12 +3,5 @@ import './main.css'
 import './index.html'
 /** websocket connection*/
 import 'webpack-hot-middleware/client?reload=true'
-
-
-const f = async args => {
-    const {a,b} = args
-    await console.log('Hello from ES6',a,b)
-    console.log('It\'s done')
-}
-
-f({a:1,b:2})
+import 'babel-register'
+import './app.js'
