7e8da4b454cdd434a7d9927e3f4af215cd4cf7b6
DziLean <gestaltldi@yandex.ru>
2018-03-29 07:56:40 +0300

    compressed, gzip

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,85 +1,89 @@
-const path = require("path")
-const webpack = require("webpack")
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
-const HTMLWebpackPlugin = require("html-webpack-plugin")
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
-const MinifyPlugin = require("babel-minify-webpack-plugin")
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const MinifyPlugin = require('babel-minify-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = env => {
-  return {
-    entry: {
-      main: ["./src/main.js"]
-    },
-    mode: "production",
-    output: {
-      filename: "[name]-bundle.js",
-      path: path.resolve(__dirname, "../dist"),
-      publicPath: "/"
-    },
-    devServer: {
-      contentBase: "dist",
-      overlay: true,
-      stats: {
-        colors: true
-      }
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader"
-            }
-          ]
+    return {
+        entry: {
+            main: ['./src/main.js']
         },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: {
-              loader: "css-loader",
-              options: {
-                minimize: true
-              }
-            }
-          })
+        mode: 'production',
+        output: {
+            filename: '[name]-bundle.js',
+            path: path.resolve(__dirname, '../dist'),
+            publicPath: '/'
         },
-        {
-          test: /\.jpg$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "images/[name].[ext]"
-              }
+        devServer: {
+            contentBase: 'dist',
+            overlay: true,
+            stats: {
+                colors: true
             }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new ExtractTextPlugin("[name].css"),
-      new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        cssProcessor: require("cssnano"),
-        cssProcessorOptions: { discardComments: { removeAll: true } },
-        canPrint: true
-      }),
-      new webpack.DefinePlugin({
-        "process.env": {
-          NODE_ENV: JSON.stringify(env.NODE_ENV)
-        }
-      }),
-      new HTMLWebpackPlugin({
-        template: "./src/index.ejs",
-        inject: true,
-        title: "Link's Journal"
-      }),
-      // new MinifyPlugin()
-      new UglifyJSPlugin()
-    ]
-  }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true
+                            }
+                        }
+                    })
+                },
+                {
+                    test: /\.jpg$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'images/[name].[ext]'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('[name].css'),
+            new OptimizeCssAssetsPlugin({
+                assetNameRegExp: /\.css$/g,
+                cssProcessor: require('cssnano'),
+                cssProcessorOptions: { discardComments: { removeAll: true } },
+                canPrint: true
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify(env.NODE_ENV)
+                }
+            }),
+            new HTMLWebpackPlugin({
+                template: './src/index.ejs',
+                inject: true,
+                title: "Link's Journal"
+            }),
+            // new MinifyPlugin()
+            new UglifyJSPlugin(),
+            new CompressionPlugin({
+                algorithm: 'gzip'
+            })
+        ]
+    }
 }
